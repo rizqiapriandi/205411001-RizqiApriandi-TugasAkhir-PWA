2eb93da9c63dbef5aa610e5a914a13991515af0f
Rizqi Apriandi <88498096+rizqiapriandi@users.noreply.github.com>
2023-07-16 17:15:40 +0700

    Initial commit

---
+++ js/lib/install.js
@@ -0,0 +1,23 @@
+// File ini digunakan untuk melakukan trigger install pada PWA
+
+export class Install {
+  /**
+   * @param {DOMElement} trigger - Triggering element
+   */
+  constructor(trigger) {
+    this._prompt;
+    this._trigger = trigger;
+  }
+
+  /**
+   * Toggle visibility of install button
+   * @param {string} action
+   */
+  toggleInstallButton(action = 'hide') {
+    if (action === 'hide') {
+      this._trigger.style.display = 'none';
+    } else {
+      this._trigger.style.display = 'block';
+    }
+  }
+}
